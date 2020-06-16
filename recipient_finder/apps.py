993bf5671440c467d1ceb39b2d316074e9c2d87f
shahid <shahid.ice.nstu@gmail.com>
2020-06-16 20:50:50 +0600

    trained model updated

--- recipient_finder/apps.py
+++ recipient_finder/apps.py
@@ -9,6 +9,6 @@ from django.conf import settings
 class RecipientFinderConfig(AppConfig):
     name = 'recipient_finder'
     # create path to models
-    path = os.path.join(settings.MODELS, 'predictor.sav')
+    path = os.path.join(settings.MODELS, 'k.sav')
     with open(path, 'rb') as pickled:
         model_ = pickle.load(pickled)
