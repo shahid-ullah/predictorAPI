0874d223b49ef5e170c4b96854ec82b49cedf88a
shahid <shahid.ice.nstu@gmail.com>
2020-06-23 07:53:02 +0600

    Updated coding convention

--- recipient_finder/views.py
+++ recipient_finder/views.py
@@ -12,6 +12,7 @@ required_features = [
     'from_officer_id', 'from_officer_designation_id', 'from_office_id', 'from_office_unit_id',
 ]
 
+
 @csrf_exempt
 def call_model(request):
     if request.method == 'POST':
@@ -40,45 +41,10 @@ def call_model(request):
         prediction = RecipientFinderConfig.model_.predict(arr)
 
         # Return response user id
-        json_formate = {'user_id': str(prediction[0])}
+        json_formate = {'user_id': int(prediction[0])}
         return JsonResponse(json_formate)
 
 
     if request.method == 'GET':
-        res = {"Required": "post method"}
+        res = {"Required": "post request"}
         return JsonResponse(res)
-
-
-
-
-# def preprocess_requested_data(json_data):
-#     print("Into preprocess requested data")
-#     print()
-#     print()
-#     dic_data = json.loads(json_data)
-#     print(f"dic_data: {dic_data}")
-
-#     if bool(dic_data):
-#         print("into if")
-#         print()
-#         print()
-#         try:
-#             # canpuring data in list
-#             input_feature_list = [int(dic_data[feature]) for feature in required_features]
-#             # print(f"input feature list: {input_feature_list}")
-#         except KeyError as key_error:
-#             res = {"missing": str(key_error)}
-#             return JsonResponse(res)
-#     else:
-#         print("into else")
-#         print()
-#         print()
-#         res = {"missing": "json data"}
-#         return JsonResponse(res)
-
-#     arr = np.array(input_feature_list)
-#     arr = arr.reshape(1, -1)
-
-#     return arr
-
-
